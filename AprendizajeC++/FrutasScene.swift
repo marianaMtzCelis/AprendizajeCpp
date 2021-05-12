06adcb1fd6efd760cffa9b86855a3cc36585dc63
Mariana Martinez Celis <marianamtzcelis19@gmail.com>
2021-05-11 22:36:20 -0500

    Added skscene file

---
+++ AprendizajeC++/FrutasScene.swift
@@ -0,0 +1,13 @@
+//
+//  FrutasScene.swift
+//  AprendizajeC++
+//
+//  Created by Mariana Martínez Celis González on 11/05/21.
+//
+
+import UIKit
+import SpriteKit
+
+class FrutasScene: SKScene {
+
+}
